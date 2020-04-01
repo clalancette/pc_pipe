77eda59e5b17eeaf2fbdcbc76c56272c11187c09
Chris Lalancette <clalancette@openrobotics.org>
2020-04-01 17:55:42 +0000

    Write out time in milliseconds when things occur.
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- pc_pipe/image_filter.py
+++ pc_pipe/image_filter.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 import sys
+import time
+
+import posix_ipc
 
 import rclpy
 from rclpy.node import Node
@@ -14,7 +17,17 @@ class MockImageFilter(Node):
         self.pub = self.create_publisher(Image, 'image_filtered', 10)
 
     def image_callback(self, msg):
-        self.get_logger().info(f'Message received [{msg.header.stamp}]')
+        #self.get_logger().info(f'Message received [{msg.header.stamp}]')
+        now_ms = int(time.time() * 1000)
+        self.get_logger().info("{} received message at {} ms".format(self.get_name().upper(), now_ms))
+
+        sem = posix_ipc.Semaphore('/pc_pipe_sem', flags=posix_ipc.O_CREAT, initial_value=1)
+        sem.acquire()
+        with open('/tmp/pc_pipe_times.csv', 'a') as outfp:
+            outfp.write('"%s SUB", %d\n' % (self.get_name().upper(), now_ms))
+        sem.release()
+        sem.close()
+
         self.pub.publish(msg)
 
 
